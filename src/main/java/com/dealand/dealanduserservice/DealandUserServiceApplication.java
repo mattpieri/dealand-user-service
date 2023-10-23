bafc27e06783e1809f70d83ad1053d2b4b828a1b
mpieri1 <mattpieri1@gmail.com>
2023-10-23 16:38:57 -0400

    Initial Commit

---
+++ src/main/java/com/dealand/dealanduserservice/DealandUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dealand.dealanduserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealandUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealandUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dealand/dealanduserservice/DealandUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dealand.dealanduserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealandUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
